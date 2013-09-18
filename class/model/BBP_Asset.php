bc84d9f33258d161ed403f4d94c06de9c69ece6e
Eddie Moya <eddie.moya@gmail.com>
2013-09-17 20:11:11 -0500

    the asset model shouldnt be enqueuing itself, thats the controllers job

--- class/model/BBP_Asset.php
+++ class/model/BBP_Asset.php
@@ -8,51 +8,44 @@ class BBP_Asset {
 	/**
 	 * @var string - script or style
 	 */
-	private $type;
+	public $type;
 
 	/**
 	 * @var string - handle to be be used for collision handling in wordpress
 	 */
-	private $handle;
+	public $handle;
 
 	/**
 	 * @var string - path to the asset
 	 */
-	private $path;
+	public $path;
 
 	/**
 	 * @var array - array of handles that should be loaded before this asset.
 	 */
-	private $dependencies = array();
+	public $dep = array();
 
 	/**
 	 * @var string - handle to be be used for collision handling in wordpress
 	 */
-	private $version = NULL;
+	public $ver = NULL;
 
 	/**
 	 * @var string - (styles only) Specify the media for which this stylesheet has been defined.
 	 * @var bool - (script only) If this parameter is true, the script is placed before the </body> end tag.
 	 */
-	private $arg = NULL;
+	public $arg = NULL;
 
-
-
+	/**
+	 * 
+	 */
 	public function __construct($config){
-	
 		$this->type = $config->type;
 		$this->handle = $config->handle;
 		$this->path = $config->path;
-		$this->dependancies = $config->dep;
-		$this->version = $config->ver;
+		$this->dep = $config->dep;
+		$this->ver = $config->ver;
 		$this->arg = $config->arg;
 
 	}
-
-	public function enqueue(){
-		$asset_uri = plugins_url($this->path, dirname(dirname(__FILE__)));
-		$enqueue_func = "wp_enqueue_" . $this->type; 
-		$enqueue_func($this->handle, $asset_uri, $this->dependancies, $this->version, $this->arg);
-	}
-
 }
