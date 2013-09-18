c9258224defa76045694c756929022133958a417
Eddie Moya <eddie.moya@gmail.com>
2013-09-17 21:07:56 -0500

    whole lot of documentation for the config parser

--- class/controller/Config_Factory.php
+++ class/controller/Config_Factory.php
@@ -2,27 +2,28 @@
 class Config_Factory {
 
 	/**
-	 * 
+	 * @var string - path to config file
 	 */
 	private $config_path;
 
 	/**
-	 * 
+	 * @var string - raw contents of config file
 	 */
 	private $config_raw;
 
 	/**
-	 * 
+	 * @var mixed - parsed content of config file. Usually a string, array, or object.
 	 */
 	private $config;
 
 	/**
-	 * 
+	 * @var array - array of all the objects instantiated from the configs
 	 */
 	private $objects;
 
+
 	/**
-	 * 
+	 * @param string $config_path - Path to config file
 	 */
 	public function __construct($config_path){
 		$this->config_path = $config_path;
@@ -30,6 +31,9 @@ class Config_Factory {
 	}
 
 	/**
+	 * Sets reads the config file contents and sets it to the config_raw property
+	 *
+	 * @uses file_get_contents();
 	 * @TODO: Exceptions handling if file doesnt exist
 	 */
 	public function load_config(){
@@ -37,13 +41,22 @@ class Config_Factory {
 	}
 
 	/**
+	 * Decodes the contents of the config file. Currently limited to JSON only.
+	 *
+	 * @uses json_decode();
+	 *
 	 * @TODO: Exception handling if not json
+	 * @TODO: Maybe add some intelligence or configurability to allow different formats of config files (interface?)
 	 */
 	public function parse_config(){
 		$this->config = json_decode($this->config_raw);
 	}
 
 	/**
+	 * Builds objects of type passed as $class, by iterating through the parsed config array|object property
+	 *
+	 * @param string $class Name of class that each object should be built with
+	 *
 	 * @TODO Exception Handling if $this->object_type doesnt exist as a class
 	 */
 	public function build($class){
@@ -54,12 +67,17 @@ class Config_Factory {
 	}
 
 	/**
+	 * Simple getter for the object resulting from the build.
+	 *
+	 * @return array - Array containing object of the type passed to $this->build
+	 *
 	 * @TODO Exception handling if $this->objects is empty;
 	 */
 	public function get_objects(){
 		return $this->objects;
 	}
-
-
-
 }
+
+
+
+
